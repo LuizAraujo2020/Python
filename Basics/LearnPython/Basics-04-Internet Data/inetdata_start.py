7bc726adec1d8c3d0a6ae3168a662874fb3767b5
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-03-15 15:30:04 -0300

    ADD: api request

--- Basics/LearnPython/Basics-04-Internet Data/inetdata_start.py
+++ Basics/LearnPython/Basics-04-Internet Data/inetdata_start.py
@@ -3,8 +3,28 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+# from urllib.request import urlopen
+# import json
+import urllib.request
+
 def main():
-    pass # this is a placeholder, do-nothing statement
+
+    # weburl = urllib.request.urlopen("https://www.google.com")
+    # print("result code:", weburl.getcode())
+    # data = weburl.read()
+    # print(data)
+    
+    url = "https://jsonplaceholder.typicode.com/todos/1"
+    # with urlopen(url) as response:
+    #     body = response.read()
+
+    #     todo_item = json.loads(body)
+    #     todo_item
+    weburl = urllib.request.urlopen(url)
+    data = weburl.read()
+    print(data)
+
+
 
 if __name__ == "__main__":
     main()
